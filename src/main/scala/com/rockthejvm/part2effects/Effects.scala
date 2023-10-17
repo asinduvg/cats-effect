984e74de09f095f1f1975647f63b578455faabb9
Asindu Vidanagamachchi <ashvg.dev@gmail.com>
2023-10-17 22:12:41 +0530

    Complete Effects:Exercises

--- src/main/scala/com/rockthejvm/part2effects/Effects.scala
+++ src/main/scala/com/rockthejvm/part2effects/Effects.scala
@@ -1,6 +1,7 @@
 package com.rockthejvm.part2effects
 
 import scala.concurrent.Future
+import scala.io.StdIn
 
 object Effects {
 
@@ -73,8 +74,77 @@ object Effects {
     42
   })
 
-  def main(args: Array[String]): Unit = {
+  /**
+   * Exercise
+   * 1. An IO which returns the current time of the system
+   * 2. An IO which measures the duration of a computation (hint: use ex1)
+   * 3. An IO which prints something to the console
+   * 4. An IO which reads a line (a string) from the std input
+   *
+   */
 
+  // 1
+  val clock = MyIO(() => System.currentTimeMillis())
+
+  // 2
+  def measure[A](computation: MyIO[A]): MyIO[Long] = for {
+    startTime <- clock
+    _ <- computation
+    finishTime <- clock
+  } yield finishTime - startTime
+
+  /*
+    clock.flatMap(
+      startTime => computation.flatMap(
+                      _ => clock.map(finishTime => finishTime - startTime)
+      )
+    )
+
+    clock.flatMap(
+      startTime => computation.flatMap(
+                        _ => MyIO(() => System.currentTimeMillis - startTime)
+      )
+    )
+
+    clock.flatMap(
+      startTime => computation.flatMap(
+                        _ => MyIO(() => System.currentTimeMillis - startTime)
+      )
+    )
+
+  computation.flatMap(lambda) = MyIO(() => lambda(__COMP__).unsafeRun())
+                              = MyIO(() => MyIO(() => System.currentTimeMillis - startTime)).unsafeRun())
+                              = MyIO(() => System.currentTimeMillis_after_computation() - startTime)
+
+  => clock.flatMap(startTime => MyIO(() => System.currentTimeMillis_after_computation() - startTime))
+  = MyIO(() => MyIO(() => System.currentTimeMillis_after_computation() - System.currentTimeMillis()).unsafeRun())
+  = MyIO(() => MyIO(() => System.currentTimeMillis_after_computation() - System.currentTimeMillis()).unsafeRun())
+  = MyIO(() => System.currentTimeMillis_after_computation() - System.currentTimeMillis())
+  */
+
+  def testTimeIO(): Unit = {
+    val test = measure(MyIO(() => Thread.sleep(1000)))
+    println(test.unsafeRun())
+  }
+
+  // 3
+  def putStrLn(line: String): MyIO[Unit] = MyIO(() => println(line))
+
+  // 4
+  val read: MyIO[String] = MyIO(() => StdIn.readLine())
+
+  def testConsole(): Unit = {
+    val program: MyIO[Unit] = for {
+      line1 <- read
+      line2 <- read
+      _ <- putStrLn(line1 + line2)
+    } yield ()
+
+    program.unsafeRun()
+  }
+
+  def main(args: Array[String]): Unit = {
+    testConsole()
   }
 
 }
