8f9e507aff5ebe6ba7f19e2ac9696edee7349828
Asindu Vidanagamachchi <ashvg.dev@gmail.com>
2023-10-20 07:43:00 +0530

    Complete IO:Error Handling

---
+++ src/main/scala/com/rockthejvm/part2effects/IOErrorHandling.scala
@@ -0,0 +1,63 @@
+package com.rockthejvm.part2effects
+
+import cats.effect.IO
+
+import scala.util.{Failure, Success, Try}
+
+object IOErrorHandling {
+
+  // IO: pure, delay, defer
+  // create failed effects
+  val aFailedCompute: IO[Int] = IO.delay(throw new RuntimeException("A FAILURE"))
+  val aFailure: IO[Int] = IO.raiseError(new RuntimeException("a proper fail"))
+
+  // handle exceptions
+  val dealWithIt = aFailure.handleErrorWith {
+    case _: RuntimeException => IO.delay(println("I'm still here"))
+    // add more cases
+  }
+
+  // turn into an Either
+  val effectAsEither: IO[Either[Throwable, Int]] = aFailure.attempt
+  // redeem: transform the failure and the success in one go
+  val resultAsString: IO[String] = aFailure.redeem(ex => s"FAIL: $ex", value => s"SUCCESS: $value")
+  // redeemWith
+  val resultAsEffect: IO[Unit] = aFailure.redeemWith(ex => IO(println(s"FAIL: $ex")), value => IO(println(s"SUCCESS: $value")))
+
+  /**
+   * Exercises
+   */
+  // 1 - construct potentially failed IOs from standard data types (Option, Try, Either)
+  def option2IO[A](option: Option[A])(ifEmpty: Throwable): IO[A] = {
+    option match
+      case Some(value) => IO.pure(value)
+      case None => IO.raiseError(ifEmpty)
+  }
+
+  def try2IO[A](aTry: Try[A]): IO[A] = {
+    aTry match
+      case Success(value) => IO(value)
+      case Failure(exception) => IO.raiseError(exception)
+  }
+
+  def either2IO[A](anEither: Either[Throwable, A]): IO[A] = {
+    anEither match
+      case Right(value) => IO(value)
+      case Left(value) => IO.raiseError(value)
+  }
+
+  // 2 - handleError, handleErrorWith
+  def handleIOError[A](io: IO[A])(handler: Throwable => A): IO[A] = {
+    io.redeem(handler, identity)
+  }
+
+  def handleErrorWith[A](io: IO[A])(handler: Throwable => IO[A]): IO[A] = {
+    io.redeemWith(handler, IO.pure)
+  }
+
+  def main(args: Array[String]): Unit = {
+    import cats.effect.unsafe.implicits.global
+    //    aFailure.unsafeRunSync()
+    resultAsEffect.unsafeRunSync()
+  }
+}
