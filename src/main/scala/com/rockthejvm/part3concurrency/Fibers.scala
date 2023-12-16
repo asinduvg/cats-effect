e6c90d9265207e9b1ad86fa5dc74a86f6ffbbff9
Asindu Vidanagamachchi <ashvg.dev@gmail.com>
2023-12-16 09:34:20 +0530

    Complete Fibers exercises
     Committer: Asindu Vidanagamachchi <ashvg.dev@gmail.com>

--- src/main/scala/com/rockthejvm/part3concurrency/Fibers.scala
+++ src/main/scala/com/rockthejvm/part3concurrency/Fibers.scala
@@ -1,9 +1,7 @@
 package com.rockthejvm.part3concurrency
 
 import cats.effect.{IO, IOApp, Fiber, Outcome}
-import cats.effect.kernel.Outcome.Succeeded
-import cats.effect.kernel.Outcome.Errored
-import cats.effect.kernel.Outcome.Canceled
+import cats.effect.Outcome.{Succeeded, Errored, Canceled}
 import scala.concurrent.duration._
 
 object Fibers extends IOApp.Simple {
@@ -73,5 +71,106 @@ object Fibers extends IOApp.Simple {
     } yield result
   }
 
-  override def run = testCancel().debugging.void
+  /** Exercises:
+    *   1. Write a function that returns an IO on another thread, and, depending
+    *      on the result of the fiber
+    *      - return the result in an IO
+    *      - if errored or cancelled, return a failed IO
+    *
+    * 2. Write a function that takes two IOs, runs them on different fibers and
+    * returns an IO with a tuple containing both results
+    *   - If both IOs complete successfully, tuple their results
+    *   - If the first IO returns an error, raise that error (ignoring the
+    *     second IO's result/error)
+    *   - If the first IO doesn't error but second IO returns an error, raise
+    *     that error
+    *   - If one (or both) cancelled, raise a RuntimeException
+    *
+    * 3. Write a function that adds a timeout to an IO:
+    *   - IO runs on an fiber
+    *   - If the timeout duration passes, then the fiber is cancelled
+    *   - the method returns an IO[A] which contains
+    *     - the original value if the computation is successful before the
+    *       timeout signal
+    *     - the exception if the computation is failed before the timeout signal
+    *     - a RuntimeException if it times out (i.e. cancelled by the timeout)
+    */
+
+  // 1
+  def processResultsFromFiber[A](io: IO[A]): IO[A] = {
+    val ioResult = for {
+      fib <- io.debugging.start
+      result <- fib.join
+    } yield result
+
+    ioResult flatMap {
+      case Succeeded(fa) => fa
+      case Errored(e)    => IO.raiseError(e)
+      case Canceled() =>
+        IO.raiseError(new RuntimeException("Computation cancelled."))
+    }
+  }
+
+  def testEx1() = {
+    val aComputation = IO("starting").debugging >> IO.sleep(1.second) >> IO(
+      "done!"
+    ).debugging >> IO(42)
+    processResultsFromFiber(aComputation).void
+  }
+
+  // 2
+  def tupleIOs[A, B](ioa: IO[A], iob: IO[B]): IO[(A, B)] = {
+    val ioResult = for {
+      fib1 <- ioa.start
+      fib2 <- iob.start
+      result1 <- fib1.join
+      result2 <- fib2.join
+    } yield (result1, result2)
+
+    ioResult.flatMap {
+      case (Succeeded(fa), Succeeded(fb)) =>
+        for {
+          a <- fa
+          b <- fb
+        } yield (a, b)
+      case (Errored(e), _) => IO.raiseError(e)
+      case (_, Errored(e)) => IO.raiseError(e)
+      case _ =>
+        IO.raiseError(new RuntimeException("Some computation cancelled."))
+    }
+  }
+
+  def testEx2() = {
+    val firstIO = IO.sleep(2.seconds) >> IO(1).debugging
+    val secondIO = IO.sleep(3.seconds) >> IO(2).debugging
+    tupleIOs(firstIO, secondIO).debugging.void
+  }
+
+  // 3
+  def timeout[A](io: IO[A], duration: FiniteDuration): IO[A] = {
+    val computation = for {
+      fib <- io.start
+      _ <- (IO.sleep(
+        duration
+      ) >> fib.cancel).start // careful - fibers can leakÒ
+//      _ <- IO.sleep(duration) >> fib.cancel
+      result <- fib.join
+    } yield result
+
+    computation.flatMap {
+      case Succeeded(fa) => fa
+      case Errored(e)    => IO.raiseError(e)
+      case Canceled() =>
+        IO.raiseError(new RuntimeException("Computation cancelled."))
+    }
+  }
+
+  def testEx3() = {
+    val aComputation = IO("starting").debugging >> IO.sleep(1.second) >> IO(
+      "done!"
+    ).debugging >> IO(42)
+    timeout(aComputation, 2.seconds).debugging.void
+  }
+
+  override def run = testEx3()
 }
